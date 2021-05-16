c65f8d522172cb8b850e770fb6bc90609abe81ff
robinwettstaedt <robinwettstadt@gmail.com>
2021-05-16 19:19:35 +0200

    working and styled. added media queries

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const userNameEl = document.querySelector('#username');
 const bioEl = document.querySelector('#bio');
 const reposEl = document.querySelector('#repos');
 const APIURL = 'https://api.github.com/users/';
+const deviceWidth = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+const modalPopUp = document.querySelector('#myModal');
 
 async function getData(username) {
     let data = await (await fetch(APIURL + username).catch(handleError)).json();
@@ -18,28 +20,53 @@ function handleError(error) {
 }
 
 function changeDOM(user, repos) {
+    if (!user.avatar_url) {
+        modalPopUp.style.display = "block";
+        return
+    }
     pictureEl.setAttribute('src', user.avatar_url);
     fullNameEl.textContent = user.name;
     userNameEl.textContent = `@${user.login}`;
     bioEl.textContent = user.bio;
 
     reposEl.innerHTML = "";
+    let repoCount = 0;
+    let maxRepoCount = 15;
+    if (deviceWidth < 721) maxRepoCount = 12;
+    if (deviceWidth < 400) maxRepoCount = 8;
+
+
     for (const repo of repos) {
+
         let repoEl = document.createElement("a");
         repoEl.classList.add("repo");
         repoEl.innerText = repo.name;
         repoEl.href = repo.html_url;
-        reposEl.appendChild(repoEl);
+        if (repoCount < maxRepoCount) {
+            reposEl.appendChild(repoEl);
+            repoCount++;
+        }
+
     }
 
 }
 
+function closeModal() {
+    modalPopUp.style.display = "none";
+}
+
 getData('robinwettstaedt');
 
-form.addEventListener('submit', (event) => {
+formEl.addEventListener('submit', (event) => {
     event.preventDefault();
 
     let input = searchEl.value
     if (input) getData(input);
 
 });
+
+window.onclick = function(event) {
+    if (event.target !== modalPopUp) {
+        modalPopUp.style.display = "none";
+    }
+}
